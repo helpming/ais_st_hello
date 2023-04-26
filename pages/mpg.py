5116ab488fe41abe64b4e6e25b3917a323360524
Yang HeeKwon <126805059+helpming@users.noreply.github.com>
2023-04-26 16:18:12 +0900

    Add files via upload

--- pages/mpg.py
+++ pages/mpg.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
+import koreanize_matplotlib
 
 st.set_page_config(
     page_title="Likelion AI School 자동차 연비 App",
